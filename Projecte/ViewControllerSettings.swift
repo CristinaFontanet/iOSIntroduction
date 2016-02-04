a8b68b5a0d9580146c1bb95ddb78142003c64bb0
1181432 <cristina.fontanet@c6s304mac04.fib.upc.es>
2016-02-04 18:53:00 +0100

    addSettingsView

---
+++ Projecte/ViewControllerSettings.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerSettings.swift
+//  Projecte
+//
+//  Created by 1181432 on 4/2/16.
+//  Copyright © 2016 FIB. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerSettings: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onButtonClicked(sender: UIButton) {
+        performSegueWithIdentifier("addManual", sender: self)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
