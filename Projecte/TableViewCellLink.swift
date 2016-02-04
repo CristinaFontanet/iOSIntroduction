d3897e60fabb690576c30a7c7ca83eb5d2265cf3
1181432 <cristina.fontanet@c6s304mac04.fib.upc.es>
2016-02-04 18:56:41 +0100

    something

---
+++ Projecte/TableViewCellLink.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellLink.swift
+//  Projecte
+//
+//  Created by 1181432 on 4/2/16.
+//  Copyright © 2016 FIB. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellLink: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
